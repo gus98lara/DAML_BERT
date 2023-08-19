908595bd6259e2e00ae26565e3d0cf775653852c
gus98lara <80791938+gus98lara@users.noreply.github.com>
2023-08-19 14:13:25 -0700

    Update data_process.py

--- HDFS/data_process.py
+++ HDFS/data_process.py
@@ -119,5 +119,5 @@ if __name__ == "__main__":
     parser(input_dir, output_dir, log_file, log_format, 'drain')
     mapping()
     hdfs_sampling(log_structured_file)
-    #generate_train_test(log_sequence_file, n=4855)
-    generate_train_test(log_sequence_file)
+    generate_train_test(log_sequence_file, n=14855)
+    #generate_train_test(log_sequence_file)
